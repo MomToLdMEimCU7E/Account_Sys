d7aa9672a4c9c643f3d7c89e69f38c0e058bfc12
Hesperus <2607536912@qq.com>
2023-04-04 20:34:58 +0800

    v1.7 账本预算实现加优化

--- Account_back/Account-BackEnd-master/src/main/resources/mustRun.sql
+++ Account_back/Account-BackEnd-master/src/main/resources/mustRun.sql
@@ -1,2 +1,57 @@
 #当我每次push后会把几张表的变动更新在这里，必须要运行
-# 这次v1.6的PUSH 建议把income payment表数据删了再插入一下
+ALTER TABLE bookkeeping DROP bookkeeping_period;
+ALTER TABLE bookkeeping DROP bookkeeping_create_date;
+ALTER TABLE bookkeeping DROP bookkeeping_end_date;
+
+CREATE TRIGGER budgetAdd
+AFTER INSERT ON bookkeeping
+FOR EACH ROW
+BEGIN			
+			INSERT INTO budget(bookkeeping_id) VALUES(NEW.bookkeeping_id);
+END;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for budget
+-- ----------------------------
+DROP TABLE IF EXISTS `budget`;
+CREATE TABLE `budget`  (
+  `budget_id` int(0) NOT NULL AUTO_INCREMENT COMMENT '预算id',
+  `bookkeeping_id` int(0) NULL DEFAULT NULL COMMENT '对应账簿id',
+  `jan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '一月预算',
+  `feb` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '二月预算',
+  `mar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '三月预算',
+  `apr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '四月预算',
+  `may` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '五月预算',
+  `jun` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '六月预算',
+  `jul` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '七月预算',
+  `aug` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '八月预算',
+  `sept` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '九月预算',
+  `oct` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '十月预算',
+  `nov` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '十一月预算',
+  `dec` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '十二月预算',
+  PRIMARY KEY (`budget_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of budget
+-- ----------------------------
+INSERT INTO `budget` VALUES (1, 1, '3500', '1000', '1000', '2000', '3000', '1500', '6000', '5000', '9000', '7000', '1200', '3100');
+INSERT INTO `budget` VALUES (2, 2, '1000', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+INSERT INTO `budget` VALUES (3, 3, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+INSERT INTO `budget` VALUES (4, 6, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+INSERT INTO `budget` VALUES (5, 10, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO `bookkeeping` VALUES (7, 1, 5, '大海', '我的账簿5', NULL, 2, 3);
+INSERT INTO `bookkeeping` VALUES (8, 2, 6, '大海', '我的账簿1', NULL, 2, 3);
+
+INSERT INTO `bookkeeping_tpye` VALUES (5, '好好学习', 'BO3-BO4-BO5-BO6-BO8-BI17-BI23-BI26-BI26');
+INSERT INTO `bookkeeping_tpye` VALUES (6, '发财', 'BO3-BO4-BO5-BO6-BO8-BI17-BI23-BI26-BI27');
+
+UPDATE bookkeeping_tpye SET bookkeeping_type_funds_id='BO3-BO4-BO5-BO6-BO7-BI17-BI23-BI26-BI27' WHERE bookkeeping_type_id=3;
+UPDATE bookkeeping_tpye SET bookkeeping_type_funds_id='BO3-BO4-BO5-BO6-BO7-BI17-BI23-BI26-BI27' WHERE bookkeeping_type_id=4;
+-- 以上为v1.7 预算功能实现 ----------------------------
