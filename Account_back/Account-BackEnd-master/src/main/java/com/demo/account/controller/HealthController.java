96d5c37b06169d047009c9724d16e60e9da77856
ILoveApricot <92791225+ILoveApricot@users.noreply.github.com>
2023-04-13 23:31:20 +0800

    上传文件

--- Account_back/Account-BackEnd-master/src/main/java/com/demo/account/controller/HealthController.java
+++ Account_back/Account-BackEnd-master/src/main/java/com/demo/account/controller/HealthController.java
@@ -3,9 +3,14 @@ package com.demo.account.controller;
 import com.demo.account.common.Result;
 import com.demo.account.entity.HealthInfo;
 import com.demo.account.service.HealthService;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/health")
@@ -41,5 +46,31 @@ public class HealthController {
     @ResponseBody
     Result<?> getHealthList(Integer uid){
         return Result.success(healthService.getHealthList(uid));
+    }
+
+    @PostMapping("/upload")
+    String upload(MultipartFile file){
+        if (file.isEmpty()){
+            return "上传失败";
+        }
+
+        String originFileName = file.getOriginalFilename();
+
+        String ext = originFileName.substring(originFileName.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString().replace("-","");
+        String fileName = uuid + ext;
+        ApplicationHome applicationHome = new ApplicationHome(this.getClass());
+        String pre = applicationHome.getDir().getParentFile().getParentFile().getAbsolutePath() +
+                "\\src\\main\\resources\\static\\images\\";
+        String path = pre + fileName;
+        try {
+            file.transferTo(new File(path));
+            return path;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return "upload fail";
+
     }
 }
--- Account_back/Account-BackEnd-master/src/main/java/com/demo/account/entity/Account.java
+++ Account_back/Account-BackEnd-master/src/main/java/com/demo/account/entity/Account.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
-
 @Data
 public class Account {
     @TableId(type = IdType.AUTO)
